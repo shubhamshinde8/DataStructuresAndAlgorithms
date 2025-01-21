4e9594e648801d79b02c1a61b367214323ddbabc
Shubham Shinde <shubhamshinde8746@gmail.com>
2025-01-20 23:28:42 -1200

    Create TakeInput.java
    Take the input from users

---
+++ src/Basics/TakeInput.java
@@ -0,0 +1,12 @@
+package Basics;
+import java.util.Scanner;
+
+public class TakeInput {
+
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       System.out.println("Enter a Number : ");
+       int n=sc.nextInt();
+       System.out.println("Your Number : "+n);
+    }
+}
